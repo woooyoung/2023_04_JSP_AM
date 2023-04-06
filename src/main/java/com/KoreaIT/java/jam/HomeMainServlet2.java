d9b4350942a1d6cfe28cf48cf8b6a8858eb49f1c
woooyoung <axdsw121@gmail.com>
2023-04-06 14:37:52 +0900

    구구단 기초, 파라미터 x

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
@@ -7,11 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/main2")
+@WebServlet("/printDan")
 public class HomeMainServlet2 extends HttpServlet {
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Hello World!!!!!!!!!!!!!!!!!!!").append(request.getContextPath());
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		
+		response.getWriter().append("8단<br>");
+
+		int dan = 8;
+		for (int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		}
 	}
 
 }
---
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet3.java
@@ -0,0 +1,17 @@
+package com.KoreaIT.java.jam;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home/main2")
+public class HomeMainServlet3 extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Hello World!!!!!!!!!!!!!!!!!!!").append(request.getContextPath());
+	}
+
+}
