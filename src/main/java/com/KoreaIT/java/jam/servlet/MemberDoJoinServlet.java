644e3772dffc61d1eb9c4c8ddcc31ebb7adcb4be
woooyoung <axdsw121@gmail.com>
2023-04-11 15:34:09 +0900

    회원가입 기능 구현, return false, 중복 제출 방지

---
+++ src/main/java/com/KoreaIT/java/jam/servlet/MemberDoJoinServlet.java
@@ -0,0 +1,77 @@
+package com.KoreaIT.java.jam.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.KoreaIT.java.jam.config.Config;
+import com.KoreaIT.java.jam.util.DBUtil;
+import com.KoreaIT.java.jam.util.SecSql;
+
+@WebServlet("/member/doJoin")
+public class MemberDoJoinServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html; charset=UTF-8");
+
+		// DB 연결
+		Connection conn = null;
+
+		try {
+			Class.forName(Config.getDBDriverClassName());
+		} catch (ClassNotFoundException e) {
+			System.out.println("예외 : 클래스가 없습니다");
+			System.out.println("프로그램을 종료합니다");
+			return;
+		}
+
+		try {
+			conn = DriverManager.getConnection(Config.getDBUrl(), Config.getDBUser(), Config.getDBPassword());
+
+			request.setCharacterEncoding("UTF-8");
+
+			String loginId = request.getParameter("loginId");
+			String loginPw = request.getParameter("loginPw");
+			String name = request.getParameter("name");
+
+			SecSql sql = SecSql.from("INSERT INTO `member`");
+			sql.append("SET regDate = NOW(),");
+			sql.append("loginId = ?,", loginId);
+			sql.append("loginPw = ?,", loginPw);
+			sql.append("`name` = ?;", name);
+
+			int id = DBUtil.insert(conn, sql);
+
+			response.getWriter().append(
+					String.format("<script>alert('%s님 가입되었습니다'); location.replace('../article/list');</script>", name));
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- src/main/webapp/jsp/home/main.jsp
+++ src/main/webapp/jsp/home/main.jsp
@@ -11,6 +11,7 @@
 	<h1>MAIN</h1>
 	<ul>
 		<li><a href="../article/list">게시물 리스트</a></li>
+		<li><a href="../member/join">회원 가입</a></li>
 	</ul>
 
 
